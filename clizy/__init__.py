42fa211abd37c8ee0984edfa1ef1ac5cca56832f
Stanislav Prokop <prokopst@users.noreply.github.com>
2018-03-20 07:52:47 +0100

    WIP implementation.

--- clizy/__init__.py
+++ clizy/__init__.py
@@ -1,10 +1,12 @@
 from argparse import ArgumentParser
 from inspect import signature, Parameter
-import sys
-from itertools import chain
 
 
 _Empty = object()
+
+
+def _normalize_string(string: str):
+    return string.replace('_', '-')
 
 
 class ArgParseArgumentBuilder:
@@ -20,48 +22,56 @@ class ArgParseArgumentBuilder:
         kwargs = {}
 
         if self.default is not _Empty:
-            dashed_name = self.name.replace('_', '-')
+            dashed_name = _normalize_string(self.name)
             args.append(f'--{dashed_name}')
+
+            kwargs['default'] = self.default
 
         if self.type is not _Empty:
             kwargs['type'] = self.type
+
+        # short_option = None
+        # for char in chain(name, (c.upper() for c in name)):
+        #     if char in used_short_options:
+        #         continue
+        #
+        #     short_option = char
 
         self._parser.add_argument(*args, **kwargs)
 
 
 class Clizy:
     def __init__(self):
-        self._argparse = ArgumentParser()
+        pass
 
-    def add_func(self, func):
+    def _prepare_function(self, parser: ArgumentParser, func):
         func_signature = signature(func)
-
-        used_short_options = set()
+        param: Parameter
 
         for param in func_signature.parameters:
-            param: Parameter
+            argument_builder = ArgParseArgumentBuilder(parser)
 
-            name: str = param.name
-            annotation = param.annotation
+            argument_builder.name = param.name
+
             default = param.default
+            if default is not Parameter.empty:
+                argument_builder.default = default
 
+            annotation = param.annotation
             if annotation == Parameter.empty:
-                param_type = str
-            else:
-                param_type = annotation
+                argument_builder.type = annotation
 
-            # argument
-            if default == Parameter.empty:
-                short_option = None
-                for char in chain(name, (c.upper() for c in name)):
-                    if char in used_short_options:
-                        continue
+            argument_builder.build()
 
-                    short_option = char
+    def run(self, *funcs):
+        parser = ArgumentParser()
 
-    def run(self, argv=None):
-        if not argv:
-            argv = sys.argv
-
-        self._argparse.parse_args(argv)
-
+        if len(funcs) > 1:
+            subparsers = parser.add_subparsers()
+            for func in funcs:
+                subparser_name = _normalize_string(func.__name__)
+                subparser = subparsers.add_parser(subparser_name)
+                self._prepare_function(subparser, func)
+        else:
+            func = funcs[0]
+            self._prepare_function(parser, func)
