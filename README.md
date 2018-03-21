8607824a1ceae6889e684634e916063f06415a73
Stanislav Prokop <prokopst@users.noreply.github.com>
2018-03-21 07:22:38 +0100

    Created Interface, Option and Argument classes.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # clizy
-Command-line interface for lazy people
+
+Command-line interface creation for lazy people using `typing`.
+
+## Quickstart
+
+```python
+# ls.py
+from clizy import run
+
+def ls(filename, long: bool=False, all: bool=False, human_readable: bool=False):
+    print(locals())
+
+
+if __name__ == '__main__':
+    run(ls)
+```
+
+When you call the script with '--help':
+
+```console
+$ python ls.py --help
+usage: ls [-h] [-l] [-a] [-H] [-L LIMIT] filename
+
+positional arguments:
+  filename
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -l, --long
+  -a, --all
+  -H, --human_readable
+  -L LIMIT, --limit LIMIT
+```
+
+
+## Todo
+
+* support `typing.List[T]` and `list`
+* parse docstring
--- clizy/__init__.py
+++ clizy/__init__.py
@@ -1,77 +1,161 @@
 from argparse import ArgumentParser
 from inspect import signature, Parameter
+from itertools import chain
+from typing import NamedTuple, Any, List
 
 
-_Empty = object()
-
-
-def _normalize_string(string: str):
+def _replace_underscores_with_dashes(string: str):
     return string.replace('_', '-')
 
 
-class ArgParseArgumentBuilder:
-    def __init__(self, parser: ArgumentParser):
-        self._parser = parser
+class Option:
+    def __init__(self, name, short_name, default, type):
+        self.name = name
+        self.short_name = short_name
+        self.default = default
+        self.type = type
 
-        self.type = _Empty
-        self.default = _Empty
-        self.name: str = _Empty
 
-    def build(self):
-        args = []
-        kwargs = {}
+class Argument(NamedTuple):
+    name: str
+    type: Any
 
-        if self.default is not _Empty:
-            dashed_name = _normalize_string(self.name)
-            args.append(f'--{dashed_name}')
 
-            kwargs['default'] = self.default
+class Interface(NamedTuple):
+    name: str
+    arguments: List[Argument]
+    options: List[Option]
 
-        if self.type is not _Empty:
-            kwargs['type'] = self.type
 
-        # short_option = None
-        # for char in chain(name, (c.upper() for c in name)):
-        #     if char in used_short_options:
-        #         continue
-        #
-        #     short_option = char
+class ClizyError(Exception):
+    pass
 
-        self._parser.add_argument(*args, **kwargs)
+
+class UnsupportedTypeError(ClizyError):
+    pass
 
 
 class Clizy:
-    def __init__(self):
-        pass
+    _UNSUPPORTED_TYPES = (int, str, bool, float)
 
-    def _prepare_function(self, parser: ArgumentParser, func):
+    def _prepare_function(self, func):
+        options = []
+        arguments = []
+
         func_signature = signature(func)
         param: Parameter
 
-        for param in func_signature.parameters:
-            argument_builder = ArgParseArgumentBuilder(parser)
-
-            argument_builder.name = param.name
+        for param_name, param in func_signature.parameters.items():
+            annotation = param.annotation
 
             default = param.default
-            if default is not Parameter.empty:
-                argument_builder.default = default
 
-            annotation = param.annotation
+            annotation_is_bool = annotation is bool
+            default_is_empty = default is Parameter.empty
+
+            if annotation_is_bool and default_is_empty:
+                default = False
+                default_is_empty = False
+
+            if isinstance(default, bool) and annotation is Parameter.empty:
+                annotation = bool
+
             if annotation == Parameter.empty:
-                argument_builder.type = annotation
+                annotation = str
 
-            argument_builder.build()
+            if annotation not in self._UNSUPPORTED_TYPES:
+                raise UnsupportedTypeError(annotation)
 
-    def run(self, *funcs):
-        parser = ArgumentParser()
+            if not default_is_empty:
+                option = Option(param.name, None, default, annotation)
+                options.append(option)
 
+            else:
+                argument = Argument(param.name, annotation)
+                arguments.append(argument)
+
+        self._assign_short_names(options)
+
+        name = _replace_underscores_with_dashes(func.__name__)
+        return Interface(name, arguments, options)
+
+    def _setup_parser(self, parser: ArgumentParser, interface: Interface):
+        for option in interface.options:
+            kwargs = {
+                'default': option.default
+            }
+
+            if option.type is bool:
+                kwargs['action'] = 'store_true'
+            else:
+                kwargs['type'] = option.type
+
+            parser.add_argument(
+                f'-{option.short_name}', f'--{option.name}', **kwargs
+            )
+
+        for argument in interface.arguments:
+            parser.add_argument(
+                argument.name, type=argument.type
+            )
+
+    def _assign_short_names(self, options):
+        used_letters = {'h'}
+
+        for option in options:
+            for letter in chain.from_iterable(zip(option.name, option.name.upper())):
+                if letter not in used_letters:
+                    used_letters.add(letter)
+                    option.short_name = letter
+                    break
+
+            if not option.short_name:
+                raise Exception
+
+    def run(self, *funcs, argv):
         if len(funcs) > 1:
-            subparsers = parser.add_subparsers()
+            parser = ArgumentParser()
+            subparsers = parser.add_subparsers(dest='clizy_subparser_name')
+            interfaces = []
+
             for func in funcs:
-                subparser_name = _normalize_string(func.__name__)
+                interface = self._prepare_function(func)
+
+                subparser_name = _replace_underscores_with_dashes(func.__name__)
                 subparser = subparsers.add_parser(subparser_name)
-                self._prepare_function(subparser, func)
+
+                self._setup_parser(subparser, interface)
+                interfaces.append(interface)
+
+            arguments = parser.parse_args(argv)
+
+            for func in funcs:
+                if func.__name__ == arguments.clizy_subparser_name:
+                    kwargs = vars(arguments)
+                    kwargs.pop('clizy_subparser_name')
+
         else:
             func = funcs[0]
-            self._prepare_function(parser, func)
+
+            interface = self._prepare_function(func)
+
+            parser = ArgumentParser(prog=interface.name)
+            self._setup_parser(parser, interface)
+
+            arguments = parser.parse_args(argv)
+            kwargs = vars(arguments)
+
+            return func(**kwargs)
+
+
+def run(*funcs, argv=None):
+    return Clizy().run(*funcs, argv=argv)
+
+
+if __name__ == '__main__':
+    def ls(filename, long: bool, all: bool, human_readable: bool, limit: int=None):
+        print(vars())
+
+    run(ls, argv=['--help'])
+    run(ls, argv=['--all', '-H', 'file.txt'])
+    run(ls, argv=['-L=42', 'file.txt'])
